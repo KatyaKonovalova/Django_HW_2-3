2c17f2d9a14e85c504cdce7dd74864c8d262909e
konov <konovalova_ekaterina_a@mail.ru>
2024-06-25 17:48:03 +0300

    migrations (0001, 0002)

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,73 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Категория",
+        help_text="Введите название категории",
+    )
+    description = models.TextField(
+        verbose_name="Описание",
+        help_text="Введите описание категории",
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Название продукта",
+        help_text="Введите название продукта",
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        verbose_name="Категория",
+        help_text="Введите название категории",
+        related_name="products",
+    )
+    price = models.FloatField(verbose_name="Цена", help_text="Введите цену товара")
+    photo = models.ImageField(
+        upload_to="catalog/photo",
+        blank=True,
+        null=True,
+        verbose_name="Фото",
+        help_text="Вставьте фотографию товара",
+    )
+    description = models.TextField(
+        verbose_name="Описание",
+        help_text="Введите описание товара",
+        blank=True,
+        null=True,
+    )
+    created_at = models.DateField(
+        verbose_name="Дата создания", help_text="Укажите дату создания", blank=True, auto_now_add=True
+    )
+    updated_at = models.DateField(
+        verbose_name="Дата последнего изменения",
+        help_text="Укажите дату последнего изменения",
+        blank=True, auto_now=True,
+    )
+    manufactured_at = models.DateField(
+        verbose_name="Дата производства продукта",
+        help_text="Введите дату производства продукта",
+        blank=True,
+    )
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+        ordering = ["name", "category", "description"]
+
+    def __str__(self):
+        return f"{self.name} - {self.price}"
